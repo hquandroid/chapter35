863d1bdaccbb1b3fa37c5cbc6158b5a2fdbc9e1f
27597581 <27597581@qq.com>
2018-04-12 08:05:27 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/hqu/cst/android/chapter35/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.hqu.cst.android.chapter35;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
